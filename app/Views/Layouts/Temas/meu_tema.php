0f66f5bf887bbd734f3cabb714630ec180667cc1
vagnerrgsb <vagnerrgsb@gmail.com>
2025-11-21 11:30:57 -0300

    Criação Layouts, partes nav_bar primaria e secundaria e footer

---
+++ app/Views/Layouts/Temas/meu_tema.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CTG Tropilha Crioula</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+        crossorigin="anonymous">
+
+    <?=  $this->renderSection("styles") ?>
+</head>
+
+<body>
+
+     <?= $this->include("Layouts/Partes/nav_bar_pri") ?>
+     <?= $this->include("Layouts/Partes/nav_bar_sec")?>
+
+  <div class="container-fluid position-relative">
+    <?= $this->renderSection("conteudo") ?>
+  </div>
+    <?=  $this->include("Layouts/Partes/footer") ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js"
+        integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y"
+        crossorigin="anonymous">
+    </script>
+
+    <?=  $this->renderSection("scripts") ?>
+</body>
+
+</html>
